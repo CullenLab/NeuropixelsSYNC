22d3637d67d361b62233072a0a52dca0bb6a3bd6
CullenLab <114096905+CullenLab@users.noreply.github.com>
2025-04-18 12:16:00 -0400

    Update deArtifact1Ch.m

--- NPXL_deArtifact/deArtifact1Ch.m
+++ NPXL_deArtifact/deArtifact1Ch.m
@@ -1,3 +1,11 @@
+%
+% Copyright (c) 2025, CullenLab, Johns Hopkins University
+% All rights reserved.
+% Created by Pum Wiboonsaksakul
+%
+% This source code is licensed under the MIT-style license found in the
+% LICENSE file in the root directory of this source tree. 
+
 %% De-artifact for ONE example channel
 function [temp,out,outf]=deArtifact1Ch(dataArray,seg,ind,params)
 
