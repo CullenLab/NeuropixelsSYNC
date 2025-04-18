8216b4aeaa829e34c0804bb84c2a3b0afd494db2
CullenLab <114096905+CullenLab@users.noreply.github.com>
2025-04-18 12:16:31 -0400

    Update findTrigRisingEdge.m

--- NPXL_deArtifact/findTrigRisingEdge.m
+++ NPXL_deArtifact/findTrigRisingEdge.m
@@ -1,3 +1,11 @@
+%
+% Copyright (c) 2025, CullenLab, Johns Hopkins University
+% All rights reserved.
+% Created by Pum Wiboonsaksakul
+%
+% This source code is licensed under the MIT-style license found in the
+% LICENSE file in the root directory of this source tree. 
+
 function ind=findTrigRisingEdge(data,thresh)
 
 ind=find(diff(data)>thresh);
