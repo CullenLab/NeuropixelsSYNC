c5a0397176beb412335605a8be0e3cf8084afad7
CullenLab <114096905+CullenLab@users.noreply.github.com>
2025-04-18 12:15:24 -0400

    Update NPXL_deArtifact.m

--- NPXL_deArtifact/NPXL_deArtifact.m
+++ NPXL_deArtifact/NPXL_deArtifact.m
@@ -1,3 +1,12 @@
+%
+% Copyright (c) 2025, CullenLab, Johns Hopkins University
+% All rights reserved.
+% Created by Pum Wiboonsaksakul
+% Includes elements adapted partly from SpikeGLX_Datafile_Tools by Jennifer Colonell
+%
+% This source code is licensed under the MIT-style license found in the
+% LICENSE file in the root directory of this source tree. 
+
 clear
 
 %% Parameters
